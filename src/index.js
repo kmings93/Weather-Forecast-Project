c3bed1919f647e87342337a25705163c4d3021c6
Saiful <saifu7bahri@gmail.com>
2022-04-07 16:31:36 +0800

    SB

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Screen from './Screens/Screen';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Screen />
   </React.StrictMode>,
   document.getElementById('root')
 );
