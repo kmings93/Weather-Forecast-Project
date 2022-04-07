77cbd23092f0c267c9f865da4b378918c4bac1de
trainingresult6361 <96628430+trainingresult6361@users.noreply.github.com>
2022-04-07 15:24:50 +0800

    Add Irene's components - UVI

--- src/index.js
+++ src/index.js
@@ -6,10 +6,21 @@ import Screen from "./Screens/Screen";
 import "./index.css";
 import App from "./App";
 
+// UVI Import 
+import APIDateTime from "../Components/Utils/APIDateTime";
+import UVIAllData from "../Components/UVI/UVIAllData";
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <Screen />
-  </React.StrictMode>,
+  
+  
+// UVI Component
+<UVIAllData dateTime={APIDateTime()} />
+  
+</React.StrictMode>,
   document.getElementById("root")
 );
