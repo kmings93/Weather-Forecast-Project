345972d3953ff16c8462997f5d1feab3fb34895a
Saiful <saifu7bahri@gmail.com>
2022-04-07 19:06:24 +0800

    SB update

--- src/Screens/Screen.js
+++ src/Screens/Screen.js
@@ -1,43 +1,53 @@
 import { API } from "../Components/API";
 import React, { useEffect, useState } from "react";
+import CSS from "../Components/psi.css";
 
 function Screen() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
 
-useEffect(() => {
+  useEffect(() => {
     // declare the async data fetching function
     const fetchData = async () => {
-        // get data from the api
-        const response = await API.get("/psi?date=2022-04-07")
-        console.log(response.data.items[0].readings);
-        let update = data;
-        console.log("update", update);
-        update.push(response.data.items[0].readings);
-        setData(update);
-        console.log("setData", data[0].psi_twenty_four_hourly);
+      // get data from the api
+      const response = await API.get("/psi?date=2022-04-07");
+      console.log(response.data.items[0].readings);
+      let update = response.data.items[0].readings;
+      setData(update);
     };
     fetchData();
-}, []);
+  }, []);
 
-    return (
-        {data ? console.log('true') : console.log('false')};
-        // <>
-        //     {/* <h1 className="main">24 hour PSI forecast</h1>
-        //     <h2 className="national">National</h2>
-        //     <h2 className="national">{data[0].psi_twenty_four_hourly.national}</h2>
-        //     <h2 className="west">West</h2>
-        //     <h2 className="west">{data[0].psi_twenty_four_hourly.west}</h2>
-        //     <h2 className="east">East</h2>
-        //     <h2 className="east">{data[0].psi_twenty_four_hourly.east}</h2>
-        //     <h2 className="north">North</h2>
-        //     <h2 className="north">{data[0].psi_twenty_four_hourly.north}</h2>
-        //     <h2 className="south">South</h2>
-        //     <h2 className="south">{data[0].psi_twenty_four_hourly.south}</h2>
-        //     <h2 className="central">Central</h2>
-        //     <h2 className="central">{data[0].psi_twenty_four_hourly.central}</h2> */}
-        //     </>
-
-    )
+  return (
+    <>
+      {data != null ? (
+        <div>
+          <div className="main">
+          <h1>24 hour PSI forecast</h1></div>
+          <div className="top">
+          <h2>National</h2>
+          <h2>{data.psi_twenty_four_hourly.national}</h2></div>
+          <div className="top">
+          <h2 className="west">West</h2>
+          <h2 className="west">{data.psi_twenty_four_hourly.west}</h2></div>
+          <div className="top">
+          <h2 className="east">East</h2>
+          <h2 className="east">{data.psi_twenty_four_hourly.east}</h2></div>
+          <div className="top">
+          <h2 className="north">North</h2>
+          <h2 className="north">{data.psi_twenty_four_hourly.north}</h2></div>
+          <div className="top">
+          <h2 className="south">South</h2>
+          <h2 className="south">{data.psi_twenty_four_hourly.south}</h2></div>
+          <div className="top">
+          <h2 className="central">Central</h2>
+          <h2 className="central">{data.psi_twenty_four_hourly.central}</h2>
+        
+        </div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
+  );
 }
-
 export default Screen;
